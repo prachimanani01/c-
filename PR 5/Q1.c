d780f88a8f6eddde6e53d79c948f6b4ecfadf480
prachi <144036679+prachimanani01@users.noreply.github.com>
2023-12-27 17:47:51 +0530

    Add files via upload

---
+++ PR 5/Q1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	
+	int n,i;
+	printf("Enter the size of array:");
+	scanf("%d",&n);
+	
+	int a[n];
+	printf("Enter array's elements : \n");
+	for(i=0;i<n;i++){
+		printf("Enter a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	printf("Negative elements from an Array : \n");
+	for(i=0;i<n;i++){
+		if(a[i]<0){
+			printf("%d\n",a[i]);	
+		 }
+		}
+	}
